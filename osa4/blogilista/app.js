dd21977fd266cb58f4139c93fea0f28dc25e4a16
LauraCoder <laura.pelkonen@student.laurea.fi>
2021-08-19 19:42:43 +0300

    Osa4 tehtävien 4.1.-4.2. palautus

---
+++ osa4/blogilista/app.js
@@ -0,0 +1,55 @@
+const config = require('./utils/config')
+const express = require('express')
+const app = express()
+const cors = require('cors')
+const blogsRouter = require('./controllers/blogs')
+const middleware = require('./utils/middleware')
+const logger = require('./utils/logger')
+const mongoose = require('mongoose')
+
+logger.info('connecting to', config.MONGODB_URI)
+
+mongoose.connect(config.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true })
+  .then(() => {
+    logger.info('connected to MongoDB')
+  })
+  .catch((error) => {
+    logger.error('error connection to MongoDB:', error.message)
+  })
+
+app.use(cors())
+app.use(express.json())
+app.use(middleware.requestLogger)
+
+app.use('/api/blogs', blogsRouter)
+
+app.use(middleware.unknownEndpoint)
+app.use(middleware.errorHandler)
+/*
+
+app.get('/api/blogs', (request, response) => {
+  Blog
+    .find({})
+    .then(blogs => {
+      response.json(blogs)
+    })
+})
+
+app.post('/api/blogs', (request, response) => {
+  const body = request.body
+  const blog = new Blog ({
+      tite: body.title,
+      author: body.author,
+      url: body.url,
+      likes: body.likes
+  })
+
+  blog
+    .save()
+    .then(savedBlog => savedBlog.toJSON())
+    .then(savedAndFormattedBlog => {
+      response.json(savedAndFormattedBlog)
+    })
+})
+*/
+module.exports = app
